e668a034f6c50ba3c4b77f6a1dd70b501b24cec6
Prasath18 <prasath.p@mstsolutions.com>
2017-08-02 11:30:47 +0530

    Update Serving.scala

--- src/main/scala/Serving.scala
+++ src/main/scala/Serving.scala
@@ -1,8 +1,27 @@
 import io.prediction.controller.LServing
+import com.github.fommil.netlib.BLAS.{getInstance => blas}
 
 class Serving extends LServing[Query, PredictedResult] {
 
   override def serve(query: Query, predictedResults: Seq[PredictedResult]): PredictedResult = {
     predictedResults.head
+    
+     def recommendProductsWithFilter(user: Int, num: Int, productIdFilter: Set[Int]) = {
+    val filteredProductFeatures = productFeatures
+      .filter { case (id, _) => !productIdFilter.contains(id) } // (*)
+    recommend(userFeatures.lookup(user).head, filteredProductFeatures, num)
+      .map(t => Rating(user, t._1, t._2))
+  }
+    
+    private def recommend(
+      recommendToFeatures: Array[Double],
+      recommendableFeatures: RDD[(Int, Array[Double])],
+      num: Int): Array[(Int, Double)] = {
+    val scored = recommendableFeatures.map { case (id, features) =>
+      (id, blas.ddot(features.length, recommendToFeatures, 1, features, 1))
+    }
+    scored.top(num)(Ordering.by(_._2))
+  }
+    
   }
 }
