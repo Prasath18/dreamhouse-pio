4eded4f9bdf728b795a2e02ad30a30851fe425a6
Prasath18 <prasath.p@mstsolutions.com>
2017-08-02 10:36:20 +0530

    Update RecommendationEngine.scala

--- src/main/scala/RecommendationEngine.scala
+++ src/main/scala/RecommendationEngine.scala
@@ -1,7 +1,7 @@
 import io.prediction.controller.EngineFactory
 import io.prediction.controller.Engine
 
-case class Query(userId: String, numResults: Int)
+case class Query(userId: String, numResults: 3)
 
 case class PredictedResult(propertyRatings: Map[String, Double])
 
